49e12c8e190ed69de4d084cc8e5cf770ac915206
Amarildo Junior <ajunior283@outlook.com>
2023-02-06 21:00:28 -0300

    Adicionando botoes para aumentar/diminuir o placar

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function alterarPlacar(lado){
-    const valorPlacar = document.getElementById(`valor${lado}`)
-    valorPlacar.textContent = (Number)(valorPlacar.textContent) + 1
+function aumentarPlacar(lado){
+  const valorPlacar = document.getElementById(`time${lado}`);
+  valorPlacar.textContent = (Number)(valorPlacar.textContent) + 1;
 } 
+
+
+const botaoUp = document.getElementsByClassName("botao-up");
+botaoUp[0].addEventListener("click", function() {
+  this.blur();
+});
+
+function diminuirPlacar(lado){
+  const valorPlacar = document.getElementById(`time${lado}`);
+  if(valorPlacar.textContent != 0) {
+    valorPlacar.textContent = (Number)(valorPlacar.textContent) - 1;
+  } 
+}
