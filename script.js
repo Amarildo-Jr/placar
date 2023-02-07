a092f21781edfcbb8a745c2fa04d7b5794f5f766
Amarildo Junior <ajunior283@outlook.com>
2023-02-06 22:51:29 -0300

    Adição de botao para reiniciar o placar

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ function diminuirPlacar(lado){
     valorPlacar.textContent = (Number)(valorPlacar.textContent) - 1;
   } 
 }
+
+function reiniciarPlacar() {
+  document.getElementById("time1").textContent = 0;
+  document.getElementById("time2").textContent = 0;
+}
