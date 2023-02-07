f6720bf98142988a8f7948b8b2d8f22f5726e6d8
Amarildo Junior <ajunior283@outlook.com>
2023-02-06 22:00:40 -0300

    Finalizando os detalhes do placar

--- script.js
+++ script.js
@@ -3,12 +3,6 @@ function aumentarPlacar(lado){
   valorPlacar.textContent = (Number)(valorPlacar.textContent) + 1;
 } 
 
-
-const botaoUp = document.getElementsByClassName("botao-up");
-botaoUp[0].addEventListener("click", function() {
-  this.blur();
-});
-
 function diminuirPlacar(lado){
   const valorPlacar = document.getElementById(`time${lado}`);
   if(valorPlacar.textContent != 0) {
